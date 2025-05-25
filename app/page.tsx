06eb6a40e4def64b331d7c57401e36ffe70d802e
Krushna Raut <krishraut2103@gmail.com>
2025-05-25 11:26:22 +0530

    notion clone with ai

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-1">
+      <h1>
+        Lets build an API productivity app
+      </h1>
+
+      <Button>hMm</Button>
+    </div>
+  );
+}

a48f22b93b3f1b61ec7178db2b9145e57501e85a
Krushna Raut <krishraut2103@gmail.com>
2025-05-29 10:37:36 +0530

    commit - 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import { type Metadata } from 'next'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+
 import './globals.css'
+import Header from '@/components/Header'
+import { ClerkProvider } from '@clerk/nextjs'
 
 
 
@@ -22,21 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+      <ClerkProvider>
+        <html lang="en">
         <body>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+          <Header />
           {children}
         </body>
       </html>
-    </ClerkProvider>
+      </ClerkProvider>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,41 @@
+'use client'
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+import { useUser } from "@clerk/nextjs"
 
 const Header = () => {
+  const {user} = useUser();
+
   return (
-    <div>Header</div>
+    <div>
+      {
+        user &&
+        (
+          <h1>{user?.firstName}{`'s`} Space </h1>
+        )
+      }
+
+      {/* Breadcrumbs */}
+
+
+      
+      <div>
+          <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+      </div>
+
+    </div>
   )
 }
 
